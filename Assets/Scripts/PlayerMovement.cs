36d03f49780048c944d8a7984d63089fadbea53a
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2022-06-03 16:19:57 -0400

    ScreenWrapAround
    Going out of screen lets the character enter frame from the opposite side of the screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,7 +24,23 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if(Touchscreen.current.primaryTouch.press.isPressed)
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    void FixedUpdate()
+    {
+        if(movementDirection == Vector3.zero) { return; }
+        // deltaTime not required for this one since it'x fixed update
+        rb.AddForce(movementDirection * forceMagnitude / 50, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maximumVelocity);
+    }
+
+    private void ProcessInput()
+    {
+
+        if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
@@ -41,12 +57,29 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if(movementDirection == Vector3.zero) { return; }
-        // deltaTime not required for this one since it'x fixed update
-        rb.AddForce(movementDirection * forceMagnitude / 50, ForceMode.Force);
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
 
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maximumVelocity);
+        if(viewportPosition.x > 1)
+        {
+            // Added 0.1 so that it doesn't try to teleport back to where it started 
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if (viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+        else if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
 }
