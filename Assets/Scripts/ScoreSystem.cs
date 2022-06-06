f6bd74735ee8faf0f8f9bebedcb157f26620ee94
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2022-06-06 15:42:51 -0400

    ScoreSystemAdded

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+
+    [SerializeField] public TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private float score;
+    private bool shouldCount = true;
+    
+    void Update()
+    {
+        if(!shouldCount) { return; }
+
+        score += Time.deltaTime * scoreMultiplier;
+
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    public int StopScoreCounting()
+    {
+        shouldCount = false;
+
+        scoreText.text = string.Empty;
+
+        return Mathf.FloorToInt(score);
+    }
+}
